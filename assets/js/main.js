a8cede4610deae0c926a0ee58b6f1c8837dcd457
Muhamad Amrul Syaifulloh <78576859+amrulsy@users.noreply.github.com>
2022-01-13 13:58:06 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,6 @@
-/**
-* Template Name: DevFolio - v4.7.1
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
